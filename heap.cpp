e8fa2051798599fa94dc447a7db4bc958c4d16a2
ju4700 <jalal.dev.design@gmail.com>
2025-05-06 22:20:52 +0600

    implement heap sort algorithm in heap.cpp

---
+++ heap.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void heapify(vector<int>& a, int n, int i){
+    int large = i;
+    int l = 2*i + 1;
+    int r = 2*i + 2;
+    if(l < n && a[l] > a[large]) large = l;
+    if(r < n && a[r] > a[large]) large = r;
+    if(large != i){
+        swap(a[i], a[large]);
+        heapify(a, n, large);
+    }
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int i; i < n; i++) cin >> a[i];
+
+    for(int i = n/2-1; i >= 0; i--) heapify(a, n, i);
+    for(int i = n-1; i >= 0; i--)
+    {
+        swap(a[0], a[i]);
+        heapify(a, i, 0);
+    }
+    
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) cout << a[i] << " ";
+    return 0; 
+}
